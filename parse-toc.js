4f10e6604c2241b82d8755530417d4469a4629aa
EvanCobb <evan.cobb@protonmail.com>
2022-01-25 00:15:49 -0600

    adds beta support for semantic version conversion

--- parse-toc.js
+++ parse-toc.js
@@ -7,7 +7,18 @@ function parseValueFromLine(line) {
     return value.join(':').trim()
 }
 
-const tocFilePath = core.getInput('tocfile', {required: true})
+// sample input:  90105
+// sample output: 9.1.5
+function convertToSemantic(supported_version) {
+    const len = supported_version.length
+    const patch = supported_version.substring(len - 2)
+    const minor = supported_version.substring(len - 4, len - 2)
+    const major = supported_version.substring(0, len - 4)
+
+    return `${Number(major)}.${Number(minor)}.${Number(patch)}`
+}
+
+const tocFilePath = core.getInput('tocfile', { required: true })
 core.debug(`Reading file ${tocFilePath}`)
 
 const lines = fs.readFileSync(tocFilePath)
@@ -19,11 +30,15 @@ core.debug(`Found lines: ${lines}`)
 const interfaceLine = lines.find(line => line.includes('## Interface:'))
 const versionLine = lines.find(line => line.includes('## Version:'))
 
-const supported_version = parseValueFromLine(interfaceLine)
 const addon_version = parseValueFromLine(versionLine)
+const supported_version = parseValueFromLine(interfaceLine)
+const supported_version_semantic = convertToSemantic(supported_version)
+
+core.debug(`Found addon_version: ${addon_version}`)
+core.setOutput('addon_version', addon_version)
 
 core.debug(`Found supported_version: ${supported_version}`)
 core.setOutput('supported_version', supported_version)
 
-core.debug(`Found addon_version: ${addon_version}`)
-core.setOutput('addon_version', addon_version)
+core.debug(`Found supported_version_semantic: ${supported_version_semantic}`)
+core.setOutput('supported_version_semantic', supported_version_semantic)
